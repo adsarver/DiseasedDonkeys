da87a8e010a63ad2b6f745b1f30442da879e6542
adsarver <andsarve15@gmail.com>
2025-04-03 14:50:24 -0400

    Renamed types file, db generation psuedo working

--- src/db-gen/gen_db.py
+++ src/db-gen/gen_db.py
@@ -1,6 +1,6 @@
 import random
-import time
-import src.gui.types as dbtypes
+from datetime import datetime
+import dbtypes
 
 class gendb():
     def __init__(self):
@@ -40,36 +40,43 @@ class gendb():
             "INPROGRESS",
             "COMPLETE"
         ]
-        with open("names.txt") as file:
-            NAMES = file.readlines(-1)
+        with open("src/db-gen/names.txt") as file:
+            self.NAMES = file.readlines(-1)
             
             
-    def gen_workers(self, special:dbtypes.Specializaion):
-        first, last = random.randint(0, len(self.NAMES)), random.randint(0, len(self.NAMES))
+    def gen_worker(self, speclist:list):
+        first, last = random.randint(0, len(self.NAMES) - 1), random.randint(0, len(self.NAMES) - 1)
         firstname, lastname = self.NAMES[first], self.NAMES[last]
         email = firstname[0] + lastname[0:3] + str(first) + "@email.com"
-        campus = self.BUILDINGS.keys[random.randint(0,2)]
+        campus = list(self.BUILDINGS.keys())[random.randint(0,2)]
+        special = speclist[random.randint(0,len(speclist) - 1)]
         
         return dbtypes.Worker(email, firstname, lastname, special, campus)
 
-    def gen_users(self):
+    def gen_user(self):
         first, last = random.randint(0, len(self.NAMES)), random.randint(0, len(self.NAMES))
         firstname, lastname = self.NAMES[first], self.NAMES[last]
         email = firstname[0] + lastname[0:3] + str(first) + "@email.com"
         
         return dbtypes.User(email, firstname, lastname)
 
-    def gen_requests(self, user:dbtypes.User, worker:dbtypes.Worker, room:dbtypes.Room):
+    def gen_request(self, userlist:dbtypes.User, workerlist:dbtypes.Worker, roomlist:dbtypes.Room):
         status = dbtypes.Status(self.STATUS[random.randint(0, 2)])
-        
-        return dbtypes.Request(status, user, time.time(), worker, room=room) # TODO: Change time
+        user = userlist[random.randint(0,len(userlist) - 1)]
+        worker = workerlist[random.randint(0,len(workerlist) - 1)]
+        room = roomlist[random.randint(0,len(roomlist) - 1)]
+        return dbtypes.Request(status, user, datetime.now(), worker, room=room) # TODO: Change time
     
-    def gen_rooms(self):
-        room_num = random.randint(100,500)
-        campus = self.BUILDINGS.keys[random.randint(0,2)]
-        possible_buildings = self.BUILDINGS[campus]
-        building = possible_buildings[random.randInit(0,len(possible_buildings))]
-        return dbtypes.Room(building, room_num)
+    def gen_rooms(self, buildings):
+        rooms = {}
+        for building in buildings:
+            for roomnum in range(100, 411):
+                if roomnum > 110 and roomnum > 210 and roomnum > 310 and roomnum > 410:
+                    continue
+                
+                rooms[building.name] = dbtypes.Room(building, roomnum)
+            
+        return rooms
     
     def init_campuses(self):
         campuses = []
@@ -80,11 +87,11 @@ class gendb():
             campus = dbtypes.Campus(campus, address)
             campuses.append(campus)
             
-            for name in self.BUILDINGS[campus]:
+            for name in self.BUILDINGS[campus.name]:
                 building = dbtypes.Building(campus, address, name)
                 buildings.append(building)
 
-        return {"campuses":campuses, "buildings":buildings}
+        return {"campus":campuses, "building": buildings}
     
     def init_statuses(self):
         return [dbtypes.Status(name) for name in self.STATUS]
@@ -92,3 +99,31 @@ class gendb():
     def init_specials(self):
         return [dbtypes.Specializaion(name) for name in self.SPECIALIZATIONS]
             
+    def generate(self):
+        status = self.init_statuses()
+        special = self.init_specials()
+        campus_map = self.init_campuses()
+        rooms = self.gen_rooms(campus_map["building"])
+        users = []
+        workers = []
+        requests = []
+                
+        for i in range(20):
+            users.append(self.gen_user())
+            workers.append(self.gen_worker(special))
+            
+        for i in range(50):
+            requests.append(self.gen_request(users, workers, list(rooms.values())))
+        
+        
+        print(status[0])
+        print(special[0])
+        print(campus_map["campus"][0])
+        print(campus_map["building"][0])
+        print(list(rooms.values())[0])
+        print(users[0])
+        print(workers[0])
+        print(requests[0])
+
+db = gendb()
+db.generate()
---
+++ src/gui/dbtypes.py
@@ -0,0 +1,141 @@
+from datetime import datetime
+
+class Specializaion:
+    def __init__(self, specialization:str, description:str=None):
+        self.id = -1
+        self.specialization = specialization.rstrip()
+        if description is not None: self.description = description.rstrip()
+        else: self.description = "None"
+        
+    def __str__(self):
+        string =   "| Specialization\n"
+        string += f"| \tID: {self.id}\n"
+        string += f"| \tSpecialization: {self.specialization}\n"
+        string += f"| \tDescription: {self.description}\n"
+        return string
+
+class Status:
+    def __init__(self, status:str, description:str=None):
+        self.id = -1
+        self.status = status
+        if description is not None: self.description = description.rstrip()
+        else: self.description = "None"
+        
+    def __str__(self):
+        string =   "| Status\n"
+        string += f"| \tID: {self.id}\n"
+        string += f"| \tStatus: {self.status}\n"
+        string += f"| \tDescription: {self.description}\n"
+        return string
+        
+class Campus:
+    def __init__(self, name:str, address:str, code=None):
+        self.id = -1
+        self.name = name.rstrip()
+        self.address = address.rstrip()
+        self.code = code
+        
+    def __str__(self):
+        string =   "| Campus\n"
+        string += f"| \tID: {self.id}\n"
+        string += f"| \tName: {self.name}\n"
+        string += f"| \tCode: {self.code}\n"
+        string += f"| \tAddress: {self.address}\n"
+        return string
+        
+class Building:
+    def __init__(self, campus:Campus, address:str, name:str=None):
+        self.id = -1
+        self.campus = campus
+        self.address = address.rstrip()
+        self.name = name.rstrip()
+        
+    def __str__(self):
+        string =   "| Building\n"
+        string += f"| \tID: {self.id}\n"
+        string += f"| \tName: {self.name}\n"
+        string += f"| \tCampus: {self.campus.name}\n"
+        string += f"| \tAddress: {self.address}\n"
+        return string
+
+class Room:
+    def __init__(self, building:Building, name:str=None, type:str=None):
+        self.id = -1
+        self.building = building
+        self.name = name
+        self.type = type
+        
+    def __str__(self):
+        string =   "| Room\n"
+        string += f"| \tID: {self.id}\n"
+        string += f"| \tName: {self.name}\n"
+        string += f"| \tBuilding: {self.building.name}\n"
+        string += f"| \tType: {self.type}\n"
+        return string
+
+class User:
+    def __init__(self, email:str, firstname:str=None, lastname:str=None):
+        self.id = -1
+        self.email = email
+        self.firstname = firstname.rstrip()
+        self.lastname = lastname.rstrip()
+        
+    def getname(self):
+        return f"{self.lastname.capitalize()}, {self.firstname.capitalize()}"
+    
+    def __str__(self):
+        string =   "| User\n"
+        string += f"| \tID: {self.id}\n"
+        string += f"| \tName: {self.lastname.capitalize()}, {self.firstname.capitalize()}\n"
+        string += f"| \tEmail: {self.email}\n"
+        return string
+        
+class Worker:
+    def __init__(self, email:str, firstname:str=None, lastname:str=None, specialization:Specializaion=None, campus:Campus=None):
+        self.id = -1
+        self.email = email
+        self.firstname = firstname.rstrip()
+        self.lastname = lastname.rstrip()
+        self.specialization = specialization
+        self.primary_campus = campus
+        
+    def getname(self):
+        return f"{self.lastname.capitalize()}, {self.firstname.capitalize()}"
+        
+    def __str__(self):
+        string =   "| Worker\n"
+        string += f"| \tID: {self.id}\n"
+        string += f"| \tName: {self.lastname.capitalize()}, {self.firstname.capitalize()}\n"
+        string += f"| \tEmail: {self.email}\n"
+        string += f"| \tSpecialization: {self.specialization.specialization}\n"
+        string += f"| \tPrimary Campus: {self.primary_campus}\n"
+        return string
+        
+class Request:
+    def __init__(self, status:Status, user:User, reqtime:datetime, assignee:Worker=None, description:str=None, comptime:datetime=None, room:Room=None):
+        self.id = -1
+        self.status = status
+        self.user = user
+        self.reqtime = reqtime
+        self.assignee = assignee
+        self.description = description
+        self.comptime = comptime
+        self.room = room
+        
+    def __str__(self):
+        reqtime = self.reqtime.strftime("%A %d/%m/%Y at %H:%M:%S")
+        string =   "| Request\n"
+        string += f"| \tID: {self.id}\n"
+        string += f"| \tStatus: {self.status.status}\n"
+        string += f"| \tRequester: {self.user.getname()}\n"
+        string += f"| \tAssignee: {self.assignee.getname()}\n"
+        string += f"| \tRoom: {self.room.name}\n"
+        string += f"| \tDescription: {self.description}\n"
+        string += f"| \tRequested on: {reqtime}\n"
+        if self.comptime is None: 
+            string += f"| \tCompleted on: {self.status.status}\n"
+        if self.comptime is not None: 
+            comptime = self.comptime.strftime("%A %d/%m/%Y at %H:%M:%S")
+            string += f"| \tCompleted on: {comptime}\n"
+        return string
+        
